88b776a4082063c3a3de904e9ec5c75244998322
Alessandro Rapiti <alessandro.rapiti@agic.cloud>
2022-07-25 17:01:10 +0200

    action and function

--- ITSUmbria2022.CSharpTips/Classes.cs
+++ ITSUmbria2022.CSharpTips/Classes.cs
@@ -8,10 +8,31 @@ namespace ITSUmbria2022.CSharpTips
 {
     public class Car
     {
+        public Func<string> Func;
+        public Func<int, string> Func2;
+        public Func<int, double, string> Func3;
+        public Func<int, double, bool, string> Func4;
+        public Func<int, double, bool, string> Func5;
         public string Plate { get; init; }
         public Car(string plate)
         {
             Plate = plate;
+            Func = ToString;
+            Func2 = Something;
+            Func3 = Something;
+            Func4 = (x, y, z) => string.Empty;
+            Func5 = (x, y, z) =>
+            {
+                return string.Empty;
+            };
+        }
+        public string Something(int x)
+        {
+            return string.Empty;
+        }
+        public string Something(int x, double t)
+        {
+            return string.Empty;
         }
     }
     public class Car3
