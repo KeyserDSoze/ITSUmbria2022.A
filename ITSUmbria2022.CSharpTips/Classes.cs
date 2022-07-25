71bb9f667f2b18875f903e19eac6b61ff2dba6fd
Alessandro Rapiti <alessandro.rapiti@agic.cloud>
2022-07-25 16:53:17 +0200

    tips for C#

---
+++ ITSUmbria2022.CSharpTips/Classes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSUmbria2022.CSharpTips
+{
+    public class Car
+    {
+        public string Plate { get; init; }
+        public Car(string plate)
+        {
+            Plate = plate;
+        }
+    }
+    public class Car3
+    {
+        public string Plate { get; init; }
+    }
+    public record Car2(string Plate, int Wheels, int Engine);
+    public struct StructuredCar
+    {
+        public double A { get; init; }
+        public double B { get; init; }
+        public double C { get; init; }
+        public double D { get; init; }
+        public static bool operator ==(StructuredCar car1, StructuredCar car2)
+        {
+            return car1.Equals(car2);
+        }
+        public static bool operator !=(StructuredCar car1, StructuredCar car2)
+        {
+            return !car1.Equals(car2);
+        }
+    }
+    public static class CarExtensions
+    {
+        public static bool Has4Wheels(this Car2 car)
+        {
+            return car.Wheels == 4;
+        }
+    }
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> enumerable, Func<T, bool> condition)
+        {
+            foreach (var item in enumerable)
+            {
+                if (condition.Invoke(item))
+                    yield return item;
+            }
+        }
+    }
+}
---
+++ ITSUmbria2022.CSharpTips/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ITSUmbria2022.CSharpTips;
+
+Console.WriteLine("Hello, World!");
+Car car = new Car("by765ou");
+Car carBis = new Car("by765ou");
+Car2 car2 = new Car2("uy786tr", 4, 1);
+Car2 car2Bis = new Car2("uy786tr", 4, 1);
+Car2 car3 = car2 with { Plate = "da435vc" };
+Car3 car4 = new Car3
+{
+    Plate = "fr456ty"
+};
+StructuredCar structuredCar = new StructuredCar { A = 1, B = 2, C = 3, D = 4 };
+StructuredCar structuredCar2 = new StructuredCar { A = 1, B = 2, C = 3, D = 4 };
+Range range = new Range(new Index(1), new Index(2));
+Range range2 = new Range(new Index(1), new Index(2));
+car2.Has4Wheels();
+Console.WriteLine(car);
+Console.WriteLine(car2);
+Console.WriteLine(car3);
+Console.WriteLine(car == carBis);
+Console.WriteLine(car2 == car2Bis);
+Console.WriteLine(structuredCar.Equals(structuredCar2));
+Console.WriteLine(range.Equals(range2));
+Console.WriteLine(structuredCar == structuredCar2);
+List<string> values = new() { "a", "a", "b" };
+foreach (var x in values.MyWhere(x => x == "a"))
+{
+    Console.WriteLine(x);
+}
+Console.ReadKey();
+
