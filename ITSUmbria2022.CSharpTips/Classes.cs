06e89340fd4f785e77d1adc785a197a0024f2aeb
Alessandro Rapiti <alessandro.rapiti@agic.cloud>
2022-07-25 17:02:02 +0200

    usings removed

--- ITSUmbria2022.CSharpTips/Classes.cs
+++ ITSUmbria2022.CSharpTips/Classes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ITSUmbria2022.CSharpTips
+﻿namespace ITSUmbria2022.CSharpTips
 {
     public class Car
     {
